68510ff94abeaa5995fab0f292d1e288510b9b0d
ewurabenafilson1 <ewurabenafilson1@gmail.com>
2025-11-22 08:56:25 -0800

    initial commit of calculator project

---
+++ index.js
@@ -0,0 +1,77 @@
+// console.log("hello world!")
+// let username = "Araba";
+// let isRunning = true;
+// let age = 100;
+
+// console.log("hello " + username);
+// console.log(`you're ${age} years old`);
+
+// if(age > 30)
+// {
+//     console.log(`you're old!`)
+// }
+    
+
+// //assignment: create three variables and create an if statement that cecks the age 
+// //fname, age and isInvited
+// //18 >= isInvited or else isNotInvited
+// //sidenote: allowed to party if the person has an invitation
+
+
+
+
+
+// let age = prompt("Enter your age: ");
+// let isInvited = prompt("Did you get an invite??(yes/no)")
+
+// function partyInvite () {
+//     if (age >= 18 && isInvited == "yes")
+// {
+//     console.log("You can join the party. Have fun!😊")
+// }
+// else if(age >= 18 && isInvited != "yes")
+// {
+//     console.log("You're of age but sorry you can't join the party!🙂")
+// }
+// else
+// {
+//     console.log("You're not of age. Sorry!🙂")
+// }    
+// }
+
+// partyInvite()
+
+
+// let num = 199
+
+
+
+// function isOdd()
+// {
+//     if(num % 2 == 0) 
+// {
+//     console.log("number is even")
+// }
+// else{
+//     console.log("number is odd")
+// }
+// }
+
+// isOdd()
+
+// function sub()
+// {
+//     let firstNum = document.getElementById("firstNum").value
+//     let secondNum = document.getElementById("secondNum").value
+//     console.log(parseInt(firstNum) - parseInt(secondNum))
+// }
+
+// // sub()
+
+// function add()
+// {
+//     let firstNum = document.getElementById("firstNum").value
+//     let secondNum = document.getElementById("secondNum").value
+//     console.log(parseInt(firstNum) + parseInt(secondNum))
+// }
+
